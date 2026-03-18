718ee7e13cb2c38f08887658d069db30231edb90
Adam <adam.ghanim@hva.nl>
2026-03-18 14:09:35 +0100

    kleuren toegevoegd aan het huis

--- turtle/adam.py
+++ turtle/adam.py
@@ -2,56 +2,70 @@ import turtle
 
 screen = turtle.Screen()
 screen.setup(width=800, height=600)
-screen.bgcolor("white")
+screen.bgcolor("lightblue")
 screen.title("Huis project")
 
 t = turtle.Turtle()
 t.speed(3)
 t.pensize(3)
 
-# beginpositie
+# basis van het huis
 t.penup()
 t.goto(-100, -100)
 t.pendown()
-
-# basis van het huis
+t.fillcolor("lightyellow")
+t.begin_fill()
 for i in range(2):
     t.forward(200)
     t.left(90)
     t.forward(120)
     t.left(90)
+t.end_fill()
 
 # dak
 t.penup()
 t.goto(-100, 20)
 t.pendown()
+t.fillcolor("red")
+t.begin_fill()
 t.goto(0, 100)
 t.goto(100, 20)
+t.goto(-100, 20)
+t.end_fill()
 
 # deur
 t.penup()
 t.goto(-20, -100)
 t.pendown()
+t.fillcolor("brown")
+t.begin_fill()
 for i in range(2):
     t.forward(40)
     t.left(90)
     t.forward(70)
     t.left(90)
+t.end_fill()
 
 # linker raam
 t.penup()
 t.goto(-75, -20)
 t.pendown()
+t.fillcolor("white")
+t.begin_fill()
 for i in range(4):
     t.forward(30)
     t.left(90)
+t.end_fill()
 
 # rechter raam
 t.penup()
 t.goto(45, -20)
 t.pendown()
+t.fillcolor("white")
+t.begin_fill()
 for i in range(4):
     t.forward(30)
     t.left(90)
+t.end_fill()
 
 turtle.done()
